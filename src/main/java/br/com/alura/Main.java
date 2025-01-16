3d087cf48abc6b27de0e83e1f04831d9b2c8cab2
Kaique <kaique.andruid@gmail.com>
2025-01-15 21:08:43 -0300

    Lendo o CSV e transformando em Produto

--- src/main/java/br/com/alura/Main.java
+++ src/main/java/br/com/alura/Main.java
@@ -1,7 +1,22 @@
 package br.com.alura;
 
+import br.com.alura.model.Produto;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Hello, World!");
+
+        List<Produto> produtos = new CsvToBeanBuilder(new FileReader("src/main/resources/products.csv"))
+                .withType(Produto.class).build().parse();
+
+        for (Produto produto : produtos) {
+            System.out.println(produto);
+        }
+
     }
 }
